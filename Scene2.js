5d55444b6c8380df7cfd592892500cec802b7691
pokhrebi <61396982+pokhrebi@users.noreply.github.com>
2022-07-17 14:38:46 -0400

    moving images

--- Scene2.js
+++ Scene2.js
@@ -7,14 +7,46 @@ class Scene2 extends Phaser.Scene {
         this.background = this.add.tileSprite(0,0, 256, 270, "background");
         this.background.setOrigin(0,0);
 
-        this.ship1 = this.add.image(256/2 - 50, 270/2, "ship")
-        this.ship2 = this.add.image(256/2, 270/2, "ship2");
-        this.ship3 = this.add.image(256/2 + 50, 270/2, "ship3");
+        this.ship1 = this.add.sprite(256/2 - 50, 270/2, "ship")
+        this.ship2 = this.add.sprite(256/2, 270/2, "ship2");
+        this.ship3 = this.add.sprite(256/2 + 50, 270/2, "ship3");
+
+        this.anims.create({
+            key: "ship1_anim",
+            frames: this.anims.generateFrameNumbers("ship"),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "ship2_anim",
+            frames: this.anims.generateFrameNumbers("ship2"),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "ship3_anim",
+            frames: this.anims.generateFrameNumbers("ship3"),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "explode",
+            frames: this.anims.generateFrameNumbers("explosion"),
+            frameRate: 20,
+            repeat: 0,
+            hideOnComplete: true
+        });
+        
+        this.ship1.play("ship1_anim");
+        this.ship2.play("ship2_anim");
+        this.ship3.play("ship3_anim");
 
         this.add.text(20, 20, "Playing Game", {
             font: "25px Arial",
              fill: "yellow"
         });
+
+
     }
     moveShip(ship, speed) {
         ship.y += speed;
