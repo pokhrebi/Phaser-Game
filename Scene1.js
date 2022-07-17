a8de796f709c1d86d55b0927c979c53067501048
pokhrebi <61396982+pokhrebi@users.noreply.github.com>
2022-07-17 10:11:11 -0400

    jumping from scene 1 to scene 2

--- Scene1.js
+++ Scene1.js
@@ -2,4 +2,8 @@ class Scene1 extends Phaser.Scene {
     constructor() {
         super("bootGame");
     }
+    create() {
+        this.add.text(20, 20, "Loading game....");
+        this.scene.start("playGame");
+    }
 }
